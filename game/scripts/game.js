396eeb2c23e5b8bbf5c2da4106801410ad9faace
Danil Shilov <94906421+JupiterHD@users.noreply.github.com>
2024-06-06 03:20:46 +0800

    Update game.js
    Bug fixes.
    
    The reviving cells did this incorrectly due to differences in the position of the cells in the even and odd rows.

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -1,14 +1,14 @@
 function start() {
-	if(is_gaming)return;
-	is_gaming = true;
+	if(game != null)return;
 	game = setInterval(step, delay);
 }
 
 
 function pause() {
-	if(!is_gaming)return;
+	if(game == null)return;
 	is_gaming = false;
 	clearInterval(game);
+	game = null;
 }
 
 
@@ -18,14 +18,15 @@ function step() {
 	{
 		let p = 0;
 		if(FIELD[(i+N*N-N)%(N*N)] == 1)p++;
-		if(FIELD[(i+N*N-N-1)%(N*N)] == 1)p++;
+		if(FIELD[(i+N*N-N-1*(parseInt(i/N)%2==1 ? -1 : 1))%(N*N)] == 1)p++;
 		if(FIELD[(i+N*N-1)%(N*N)] == 1)p++;
 		if(FIELD[(i+1)%(N*N)] == 1)p++;
 		if(FIELD[(i+N)%(N*N)] == 1)p++;
-		if(FIELD[(i+N-1)%(N*N)] == 1)p++;
+		if(FIELD[(i+N-1*(parseInt(i/N)%2==1 ? -1 : 1))%(N*N)] == 1)p++;
 		arr.push(p);
 	}
-	for(let i = 0; i < FIELD.length; i++)
-		if(arr[i] == 2 || arr[i] == 3)revive(i);
+	for(let i = 0; i < arr.length; i++)
+		if(settings[arr[i]] == 1)revive(i);
 		else kill(i);
+	if(stopgame)extinction();
 }
